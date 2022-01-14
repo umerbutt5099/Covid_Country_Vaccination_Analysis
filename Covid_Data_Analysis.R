7b9596d75ad423a35f5c0880fceddfac43424aa4
umerbutt5099 <umerbutt5099@gmail.com>
2022-01-14 23:22:53 +0500

    Just updated  but still discrete Vaccine Distribution is not visible.

--- Covid_Data_Analysis.R
+++ Covid_Data_Analysis.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(ggplot2)
 file<-read.csv("country_vaccinations.csv")
 head(file)
 countrytable<-as.data.frame(table(file$country))
@@ -6,10 +7,30 @@ countrytable
 subset(countrytable, countrytable$Var1!= "England" & countrytable$Var1!= "Scotland" & countrytable$Var1!= "Wales" & countrytable$Var1!= "Northern Ireland" )
 vaccines<-as.data.frame(table(file$vaccines))
 vaccinebycountry<-as.data.frame(table(file$vaccines,file$country))
-vaccinebycountry %>%  rename(   vaccine = Var1, country = Var2)
-vaccinebycountry<-vaccinebycountry %>%  rename(   Vaccine = Vaccine, region = Country)
-map_data<- left_join(map_data,vaccinebycountry,by="region")
-mapdata1<-map_data%>% filter(!is.na(map_data$Freq))
-map1<-ggplot(mapdata1,aes(x=long,y=lat,group=group))+geom_polygon(aes(fill=mapdata1$Freq),color="black")
-map1
- 
+colnames(vaccinebycountry)<-c('Vaccine','region','Frequency')
+
+
+WorldData <- map_data('world') %>% filter(region != "Antarctica") %>% fortify
+
+
+p <- ggplot() +
+  geom_map(data = WorldData, map = WorldData,
+           aes(x = long, y = lat, group = group, map_id=region),
+           fill = "white", colour = "blue", size=0.5) + 
+  geom_map(data = vaccinebycountry, map=WorldData,
+           aes(map_id=region),
+           colour="green", size=0.5) +
+  coord_map("rectangular", lat0=0, xlim=c(-180,180), ylim=c(-60, 90)) +
+  scale_fill_continuous( ) +
+  scale_y_continuous() +
+  scale_x_continuous() +
+  labs(fill="Vaccines", title="Covid Vaccine Distribution", x="Longitude", y="Latitude") +
+  theme_bw()
+
+p
+
+
+
+
+
+
